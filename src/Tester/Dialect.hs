09893b4e7e3a774cb1c597360f6d76769f4689ad
TheBizzle <jason.bertsche@gmail.com>
2014-08-02 00:23:41 -0500

    Adding the test-selection DSL/dialect

---
+++ src/Tester/Dialect.hs
@@ -0,0 +1,43 @@
+module Tester.Dialect(CellBox, FlagCell, ToggleFlag, andAlso, butAlso, excludingTo, notRunning, runningTo) where
+
+  data ToggleFlag
+    = Talkative
+    | StackTrace deriving (Show)
+
+  data FlagCell
+    = ToggleCell { flag :: ToggleFlag }
+    | DontRun    { num :: Int }
+    | Run        { num :: Int } deriving (Show)
+
+  data FlagCells
+    = FlagCells {
+        cells :: [FlagCell]
+      } deriving (Show)
+
+  notRunning :: Int -> FlagCell
+  notRunning x = DontRun x
+
+  runningTo :: Int -> Int -> FlagCells
+  runningTo x y = FlagCells (fmap (\z -> Run z) [x..y])
+
+  excludingTo :: Int -> Int -> FlagCells
+  excludingTo x y = FlagCells (fmap (\z -> DontRun z) [x..y])
+
+  andAlso, butAlso :: (CellBox a, CellBox b) => a -> b -> FlagCells
+  andAlso x y = FlagCells ((unbox x) ++ (unbox y))
+  butAlso = andAlso
+
+  class CellBox x where
+    unbox :: x -> [FlagCell]
+
+  instance CellBox FlagCells where
+    unbox (FlagCells cells) = cells
+
+  instance CellBox FlagCell where
+    unbox cell = [cell]
+
+  instance CellBox ToggleFlag where
+    unbox flag = unbox (ToggleCell flag)
+
+  infixl 6 `runningTo`,`excludingTo`
+  infixl 2 `andAlso`,`butAlso`
