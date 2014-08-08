99fbeebe4886f58e54c4143db8b769592529e06e
TheBizzle <jason.bertsche@gmail.com>
2014-08-07 22:56:57 -0500

    Providing an ordering for my `FlagCell`s
    This is be greatly useful to anyone attempting to optimize this a DSL
    string, such as...

--- src/Tester/Dialect.hs
+++ src/Tester/Dialect.hs
@@ -39,5 +39,18 @@ module Tester.Dialect(CellBox, FlagCell(..), FlagCells(..), ToggleFlag(..), andA
   instance CellBox ToggleFlag where
     unbox flag = unbox (ToggleCell flag)
 
+  -- Example: [DontRun 0, Run 1, DontRun 2, Run 2, Run 3, DontRun 5, StackTrace, Talkative]
+  instance Ord FlagCell where
+    (Run        a)         <= (Run        b)          = a <= b
+    (DontRun    a)         <= (DontRun    b)          = a <= b
+    (ToggleCell Talkative) <= (ToggleCell StackTrace) = False
+    (ToggleCell _)         <= (ToggleCell _)          = True
+    (ToggleCell _)         <= (DontRun    _)          = False
+    (ToggleCell _)         <= (Run        _)          = False
+    (DontRun    _)         <= (ToggleCell _)          = True
+    (Run        _)         <= (ToggleCell _)          = True
+    (Run        a)         <= (DontRun    b)          = a < b
+    (DontRun    a)         <= (Run        b)          = a <= b
+
   infixl 6 `runningTo`,`excludingTo`
   infixl 2 `andAlso`,`butAlso`
